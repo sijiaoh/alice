196f7e59041537ec11601719d8165b3d21efa84b
Sijia Wang <sijiaoh@outlook.com>
2021-06-14 03:06:13 +0900

    Add default-ts-configs dependencies.

--- packages/default-ts-configs/src/index.ts
+++ packages/default-ts-configs/src/index.ts
@@ -7,12 +7,13 @@ import { Command } from "commander";
 const thisPath = path.join("..", "default-ts-configs");
 
 interface Package {
-  scripts: { [key: string]: string | undefined };
+  scripts?: { [key: string]: string | undefined };
+  dependencies?: { [key: string]: string | undefined };
 }
 
 const program = new Command();
 
-const commands = ({ force }: { force?: boolean } = {}) => {
+const scripts = ({ force }: { force?: boolean } = {}) => {
   const packageJsonStr = "package.json";
 
   const srcPackageJson = fs
@@ -22,9 +23,32 @@ const commands = ({ force }: { force?: boolean } = {}) => {
   const destPackageJson = fs.readFileSync(packageJsonStr).toString();
   const destPackage: Package = JSON.parse(destPackageJson);
 
+  if (!srcPackage.scripts) return;
+
+  destPackage.scripts ||= {};
   Object.keys(srcPackage.scripts).forEach((key) => {
-    if (!destPackage.scripts[key] || force)
-      destPackage.scripts[key] = srcPackage.scripts[key];
+    if (!destPackage.scripts![key] || force)
+      destPackage.scripts![key] = srcPackage.scripts![key];
+  });
+
+  fs.writeFileSync(packageJsonStr, JSON.stringify(destPackage, null, 2) + EOL);
+};
+
+const dependencies = () => {
+  const packageJsonStr = "package.json";
+
+  const srcPackageJson = fs
+    .readFileSync(path.join(thisPath, packageJsonStr))
+    .toString();
+  const srcPackage: Package = JSON.parse(srcPackageJson);
+  const destPackageJson = fs.readFileSync(packageJsonStr).toString();
+  const destPackage: Package = JSON.parse(destPackageJson);
+
+  if (!srcPackage.dependencies) return;
+
+  destPackage.dependencies ||= {};
+  Object.keys(srcPackage.dependencies).forEach((key) => {
+    destPackage.dependencies![key] = srcPackage.dependencies![key];
   });
 
   fs.writeFileSync(packageJsonStr, JSON.stringify(destPackage, null, 2) + EOL);
@@ -52,18 +76,24 @@ const link = ({ force }: { force?: boolean } = {}) => {
 
 program
   .command("bootstrap")
-  .description("Run commands and link")
-  .option("-f, --force", "Pass force option to commands.")
+  .description("Run scripts and link")
+  .option("-f, --force", "Pass force option to scripts.")
   .action(({ force }) => {
-    commands({ force });
+    scripts({ force });
+    dependencies();
     link({ force });
   });
 
 program
-  .command("commands")
-  .description("Add default yarn commands to package.json.")
-  .option("-f, --force", "Overwrite commands")
-  .action(commands);
+  .command("scripts")
+  .description("Add default yarn scripts to package.json.")
+  .option("-f, --force", "Overwrite scripts")
+  .action(scripts);
+
+program
+  .command("dependencies")
+  .description("Add default yarn dependencies to package.json.")
+  .action(dependencies);
 
 program
   .command("link")
--- packages/editor-core/.eslintrc.json.js
+++ packages/editor-core/.eslintrc.json.js
@@ -1,1 +1,1 @@
-../../eslint/.eslintrc.json.js
+../default-ts-configs/.eslintrc.json.js
