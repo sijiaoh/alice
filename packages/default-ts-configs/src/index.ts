952dbd29a157974733a644516f4cff3a71292e35
Sijia Wang <sijiaoh@outlook.com>
2021-06-16 07:26:41 +0900

    Add nodeVersion command.

--- packages/default-ts-configs/src/index.ts
+++ packages/default-ts-configs/src/index.ts
@@ -7,11 +7,28 @@ import { Command } from "commander";
 const thisPath = path.join("..", "default-ts-configs");
 
 interface Package {
+  engines?: { node: string };
   scripts?: { [key: string]: string | undefined };
   dependencies?: { [key: string]: string | undefined };
 }
 
 const program = new Command();
+
+const nodeVersion = () => {
+  const packageJsonStr = "package.json";
+
+  const srcPackageJson = fs
+    .readFileSync(path.join(thisPath, packageJsonStr))
+    .toString();
+  const srcPackage: Package = JSON.parse(srcPackageJson);
+  const destPackageJson = fs.readFileSync(packageJsonStr).toString();
+  const destPackage: Package = JSON.parse(destPackageJson);
+
+  if (!srcPackage.engines) return;
+  destPackage.engines = srcPackage.engines;
+
+  fs.writeFileSync(packageJsonStr, JSON.stringify(destPackage, null, 2) + EOL);
+};
 
 const scripts = ({ force }: { force?: boolean } = {}) => {
   const packageJsonStr = "package.json";
@@ -79,10 +96,18 @@ program
   .description("Run scripts and link")
   .option("-f, --force", "Pass force option to scripts.")
   .action(({ force }) => {
+    nodeVersion();
     scripts({ force });
     dependencies();
     link({ force });
   });
+
+program
+  .command("nodeVersion")
+  .description(
+    "Use src package.json to override the dest package.json node version."
+  )
+  .action(nodeVersion);
 
 program
   .command("scripts")
