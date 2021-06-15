ccd4c51a50bc4f5b9e0cb80e30b24e0f88a3ca23
Sijia Wang <sijiaoh@outlook.com>
2021-06-16 08:07:50 +0900

    Remove . from test comments.

--- packages/dotenv-generator/src/generate.test.ts
+++ packages/dotenv-generator/src/generate.test.ts
@@ -2,12 +2,12 @@ import { generate } from "./generate";
 
 describe("generate", () => {
   describe("with legal env", () => {
-    it("generate env.ts.", () => {
+    it("generate env.ts", () => {
       expect(generate()).toMatchSnapshot();
     });
   });
   describe("with illegal env", () => {
-    it("throw error.", () => {
+    it("throw error", () => {
       expect(() => {
         generate("test2");
       }).toThrow();
