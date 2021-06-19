449a504c0fad0513b6d1908c53ac07549365b7bb
Sijia Wang <sijiaoh@outlook.com>
2021-06-20 07:30:36 +0900

    Fix dotenv-generator test.

--- packages/dotenv-generator/src/generate.test.ts
+++ packages/dotenv-generator/src/generate.test.ts
@@ -2,15 +2,13 @@ import { generate } from './generate';
 
 describe('generate', () => {
   describe('with legal env', () => {
-    it('generate env.ts', () => {
-      expect(generate()).toMatchSnapshot();
+    it('generate env.ts', async () => {
+      expect(await generate()).toMatchSnapshot();
     });
   });
   describe('with illegal env', () => {
-    it('throw error', () => {
-      expect(() => {
-        generate('test2');
-      }).toThrow();
+    it('throw error', async () => {
+      await expect(generate('test2')).rejects.toThrow();
     });
   });
 });
