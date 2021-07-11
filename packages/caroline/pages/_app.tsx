773ce7d6d0b94e9031a6c31b2e5056ff1c730163
Sijia Wang <sijiaoh@outlook.com>
2021-07-12 01:17:44 +0900

    Change js to ts.

---
+++ packages/caroline/pages/_app.tsx
@@ -0,0 +1,5 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
---
+++ packages/caroline/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Caroline</title>
+      </Head>
+      <div>Hello</div>
+    </>
+  );
+}
