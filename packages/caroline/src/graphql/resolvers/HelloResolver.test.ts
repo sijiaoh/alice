8cd672f399c328e23fb457b3695942c7c66f78bb
Sijia Wang <sijiaoh@outlook.com>
2021-07-15 09:44:10 +0900

    Add graphql-codegen.

--- packages/caroline/src/graphql/resolvers/HelloResolver.test.ts
+++ packages/caroline/src/graphql/resolvers/HelloResolver.test.ts
@@ -1,11 +1,12 @@
 import { apolloServer } from '../apolloServer';
 import { HelloResolver } from './HelloResolver';
+import { HelloDocument } from '../../generated/graphql';
 
 describe(HelloResolver.name, () => {
   describe(HelloResolver.prototype.hello.name, () => {
     it('return hello world string', async () => {
       const res = await apolloServer.executeOperation({
-        query: `{hello}`,
+        query: HelloDocument,
       });
       expect(res.data?.hello).toBe('hello world');
     });
---
+++ scripts/all-graphql-queries.js
@@ -0,0 +1,154 @@
+module.exports = {
+  plugin: (schema) => {
+    const toUpperCamel = (str) => {
+      return str.replace(/^./, (c) => c.toUpperCase());
+    };
+
+    const text = schema.extensions.sources
+      .map((source) => source.body)
+      .join('\n');
+
+    const oneline = text.replace(/\n/g, '');
+
+    const typeLines = oneline
+      .match(/type (.*?)}/g)
+      .filter((line) => !line.match(/type [Mutation]/));
+
+    const mutationLine = oneline
+      .match(/type (.*?)}/g)
+      .find((line) => line.match(/type [Mutation]/));
+
+    /*
+      [{
+        TypeName: { name: 'TypeName', props: [
+          {
+            name: 'propName',
+            name: 'PropType(!)',
+          }
+        ]}
+      }]
+    */
+    const types = typeLines.reduce((obj, line) => {
+      const name = line.match(/type ([^\s]*) {/)[1];
+      const matches = line ? [...line.matchAll(/([^\s]*): ([^\s}$]*)/g)] : [];
+      const props = matches.map((match) => ({
+        name: match[1],
+        type: match[2],
+      }));
+      return { ...obj, [name]: { name, props } };
+    }, {});
+
+    /*
+      [
+        {
+          "name": "mutationName",
+          "props": [
+            {
+              "name": "propName",
+              "type": "PropType(!)"
+            },
+          ],
+          "returnType": "ReturnType(!)"
+        },
+      ]
+    */
+    const matches = mutationLine
+      ? [...mutationLine.matchAll(/([^\s]*?\(.*?\)): ([^\s}$]*)/g)]
+      : [];
+    let mutations = matches.map((match) => {
+      const [, name, propsStr] = [...match[1].match(/(.*?)\((.*)\)/)];
+      const propStrs = propsStr.split(', ');
+      const props = propStrs.map((str) => {
+        const [propName, propType] = str.split(': ');
+        return { name: propName, type: propType };
+      });
+      return {
+        name,
+        props,
+        returnType: match[2],
+      };
+    });
+    // No props.
+    const matches2 = mutationLine
+      ? [...mutationLine.matchAll(/ {2}([^\s:]*?): ([^\s}$]*)/g)]
+      : [];
+    const mutations2 = matches2
+      .filter((match) => !match[1].includes('('))
+      .map((match) => {
+        return {
+          name: match[1],
+          props: null,
+          returnType: match[2],
+        };
+      });
+
+    mutations = [...mutations, ...mutations2];
+
+    const queries = types.Query;
+
+    const queriesText = queries.props
+      .map(({ name, type }) => {
+        const typeName = type.replace(/!$/, '');
+        let props = '';
+        if (types[typeName]) {
+          props = `{
+          ${types[typeName].props
+            .map((prop) => {
+              return prop.name;
+            })
+            .join(' ')}
+        }`;
+        }
+
+        return `
+        query ${toUpperCamel(name)} {
+          ${name}${props}
+        }
+      `;
+      })
+      .join('\n');
+
+    const mutationsText = mutations
+      .map((mutation) => {
+        let props = '';
+        const returnTypeName = mutation.returnType.replace(/!$/, '');
+        const returnType = types[returnTypeName];
+        if (returnType) {
+          props = `{
+          ${returnType.props.map((type) => type.name).join(' ')}
+        }`;
+        }
+
+        let parameters = '';
+        if (mutation.props) {
+          parameters += '(';
+          parameters += mutation.props
+            .map((prop) => {
+              return `$${prop.name}: ${prop.type}`;
+            })
+            .join(', ');
+          parameters += ')';
+        }
+
+        let args = '';
+        if (mutation.props) {
+          args += '(';
+          args += mutation.props
+            .map((prop) => {
+              return `${prop.name}: $${prop.name}`;
+            })
+            .join(', ');
+          args += ')';
+        }
+
+        return `
+        mutation ${toUpperCamel(mutation.name)}${parameters} {
+          ${mutation.name}${args}${props}
+        }
+      `;
+      })
+      .join('\n');
+
+    return [queriesText, mutationsText].join('\n');
+  },
+};
