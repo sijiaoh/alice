626c5ca689bd5846b8e604906976531bce04bd65
Sijia Wang <sijiaoh@outlook.com>
2021-07-17 12:04:22 +0900

    Format codes.

--- packages/caroline/src/graphql/resolvers/HelloResolver.test.ts
+++ packages/caroline/src/graphql/resolvers/HelloResolver.test.ts
@@ -1,6 +1,6 @@
+import { HelloDocument } from '../../generated/graphql';
 import { apolloServer } from '../apolloServer';
 import { HelloResolver } from './HelloResolver';
-import { HelloDocument } from '../../generated/graphql';
 
 describe(HelloResolver.name, () => {
   describe(HelloResolver.prototype.hello.name, () => {
