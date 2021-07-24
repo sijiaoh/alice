44fd7fca43aa17732299febbfa8cc3c3a49ab8b0
Sijia Wang <sijiaoh@outlook.com>
2021-07-25 05:11:54 +0900

    Add sanitize.css.

--- packages/caroline/src/pages/_app.tsx
+++ packages/caroline/src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router';
 
+import 'sanitize.css';
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
